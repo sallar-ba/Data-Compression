5d466be7ef01fda52170573e182b1ff41b347de3
Huzaifa <huzaifailyas1122@gmail.com>
2022-06-26 18:57:13 +0500

    Find Compression Ratio and commented the whole huffman tree functions and displayed the optimised codes perfectly

--- Project/Project/Compression.cpp
+++ Project/Project/Compression.cpp
@@ -25,7 +25,7 @@ void Compression::readFromFile()
 		while (!readFile.eof()) // Run Till End of File
 		{
 			// Copy Data From readFile to Content String
-			while (getline(readFile, content) )
+			while (getline(readFile, content))
 			{
 				// getting the whole text file size 
 				sizeOfData = content.length();
@@ -103,7 +103,7 @@ void Compression::calculatingFreq(int i = 2)
 	}
 
 
-	
+
 }
 //----------------------------------------------------------
 void Compression::PrintCharWithFreq()
@@ -134,55 +134,83 @@ void Compression::splitArray()
 	{
 		Huff[i].insert(Array[i].Symbol, Array[i].Freq);
 	}
-	
+	/* Passing to the function which encodes huffman(Makes Tree)
+	and decodes into optimized codes as well */
 	Decode(Huff, Size);
 }
 //----------------------------------------------------
 HuffmanTree* Compression::Tree(priority_queue<HuffmanTree*, vector<HuffmanTree*>, compareNode> PQue)
 {
+	/* This function makes tree untill the pQue size becomes 1
+	which will be the root node of the whole tree */
 	while (PQue.size() != 1)// function is used to get size of the priority queue
 	{
+		// saving the top node in left
 		HuffmanTree* Left = PQue.top(); //function is used to reference the top element of the priority queue
 
+		// Poping the top node from Pque
 		PQue.pop();
 
+		//saving the second top node in right node
 		HuffmanTree* Right = PQue.top(); //function is used to reference the top element of the priority queue
 
+		// poping that from Pque
 		PQue.pop();
 		Data Check;
-		Check.insert('@', Left->Huff.Freq + Right->Huff.Freq);
-		HuffmanTree* newNode = new HuffmanTree(Check);// Assigning random character after addition
+		Check.insert('@', Left->Huff.Freq + Right->Huff.Freq);// making new data node 
+		HuffmanTree* newNode = new HuffmanTree(Check);
+		// initiailizing the new node left and right child with the poped nodes 
 		newNode->Left = Left;
 		newNode->Right = Right;
 
+		// Pushing the new node into the pQue
 		PQue.push(newNode);
 	}
 
+	/* at the end the root node will be	returned
+	from where we will decode the tree in binary codes */
 	return PQue.top();
 
 }
 //---------------------------------------------------------------------
-void Compression::Display(HuffmanTree* root, int Array[], int top)
+void Compression::Display(HuffmanTree* root, int Arr[], int top)
 {
+	/* If the node have the left element till the symbol initialize the Array[top]
+		0 (because according too the huffman algorithm ) */
 	if (root->Left) //assign 0 to the left child path
 	{
-		Array[top] = 0;
-		Display(root->Left, Array, top + 1);
+		Arr[top] = 0;
+		Display(root->Left, Arr, top + 1);// recursively call with top + 1 till the leaf node
 	}
+	/* If the node have the Right element till the symbol initialize the Array[top]
+		1 (because according too the huffman algorithm ) */
 	if (root->Right)// assign 1 to the right child path
 	{
-		Array[top] = 1;
-		Display(root->Right, Array, top + 1);
+		Arr[top] = 1;
+		Display(root->Right, Arr, top + 1);// recursively call with top + 1 till the leaf node
 	}
+
+	/* if the node doesnot contain leftand right child
+	then it will be the leaf node then print the saved codes in Array */
 	if (!root->Left && !root->Right)// if the leaf node appear with no left and right child
 	{
-		//cout << root->Symbol << " ";
+		int counter = 0;
+		cout << root->Huff.Symbol << " ";
 		for (int i = 0; i < top; i++)
 		{
-			cout << Array[i];
+			cout << Arr[i];
+			counter++;// counting the optimised codes per symbols
 		}
-		cout << " ";
-		//cout << endl;
+		for (int i = 0; i < Array.size(); i++)
+		{
+			// checking for the symbol 
+			if (root->Huff.Symbol == Array[i].Symbol)
+			{
+				compressionRatio += Array[i].Freq * counter;// if found multiply with its frequency and add in compression Ratio
+			}
+		}
+		//cout << " ";
+		cout << endl;
 	}
 }
 //----------------------------------------------------------------
@@ -198,5 +226,7 @@ void Compression::Decode(Data Huff[], int size)
 	HuffmanTree* root = Tree(Pq);// making huffman encoding tree
 	int arr[Max_Size], top = 0;
 	Display(root, arr, top);// print the optimized codes
+	cout << "Compression Ratio is " << compressionRatio / Array.size() << endl;// final compression ratio 
+
 }
 //----------------------------------------------------------------------
