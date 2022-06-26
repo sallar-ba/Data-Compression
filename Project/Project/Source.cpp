a115725ec50339878d62e054825ed589af754842
Huzaifa <huzaifailyas1122@gmail.com>
2022-06-26 19:01:21 +0500

    Added the driver.cpp

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -3,9 +3,28 @@ using namespace std;
 
 int main()
 {
-	string Name = "Huffman.txt";
+	string Name;
+	int choice;
+	cout << "1- File Locatio\n2- File Name" << endl;
+	cout << "choice : ";
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		cout << "Enter File Location : ";
+		cin >> Name;
+		break;
+	case 2:
+		cout << "Enter File Name : ";
+		cin >> Name;
+		break;
+	default:
+		break;
+	}
+	//cout << "Enter File Location : ";
+	//cin >> Name;
 	Compression C1(Name);
-	C1.Compress(); 
+	C1.Compress();
 
 	system("pause>0");
 	return 0;
